cb35c9d1dbe0e9be79bc7e1be056f8e44755498a
EpicPix <epicpixminecraft@gmail.com>
2023-03-11 22:25:22 +0100

    Reformat file

--- src/main.c
+++ src/main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <zip.h>
 
-int main(int argc, char** argv) {
-    if(argc < 2) {
-        printf("Usage; %s <datapack>\n", argv[0]);
-        return 1;
-    }
-    printf("Hello world! %s\n", argv[1]);
-    zip_t* zip = zip_open(argv[1], ZIP_RDONLY, NULL);
-    printf("Zip: %p\n", zip);
-    return 0;
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("Usage; %s <datapack>\n", argv[0]);
+    return 1;
+  }
+  printf("Hello world! %s\n", argv[1]);
+  zip_t *zip = zip_open(argv[1], ZIP_RDONLY, NULL);
+  printf("ZIP: %p\n", zip);
+  return 0;
 }
