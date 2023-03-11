2f9495a005ab71873b5acb4225ce4bafe6c75470
EpicPix <epicpixminecraft@gmail.com>
2023-03-11 22:49:06 +0100

    Read all files now too

--- src/main.c
+++ src/main.c
@@ -16,7 +16,11 @@ int main(int argc, char **argv) {
   for (int64_t i = 0; i < entry_count; i++) {
     zip_stat_t stat;
     zip_stat_index(zip, i, 0, &stat);
-    printf("%s\n", stat.name);
+    zip_file_t *file = zip_fopen_index(zip, i, 0);
+    char *content = malloc(stat.size + 1);
+    zip_fread(file, content, stat.size);
+    content[stat.size] = '\0';
+    free(content);
   }
 
   zip_close(zip);
