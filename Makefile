ef59063a2b9445019135b48ff412fa107db03738
EpicPix <epicpixminecraft@gmail.com>
2023-03-11 22:05:18 +0100

    Try loading zip file

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ clean:
 SRC = $(wildcard src/*.c)
 
 analyzer: $(SRC)
-	gcc -o $@ $^
+	gcc -o $@ $^ -lzip
--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
+#include <json-c/json.h>
 #include <stdio.h>
+#include <zip.h>
 
-int main(int atgc, char** argv) {
-    printf("Hello world!\n");
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        printf("Usage; %s <datapack>\n", argv[0]);
+        return 1;
+    }
+    printf("Hello world! %s\n", argv[1]);
+    zip_t* zip = zip_open(argv[1], ZIP_RDONLY, NULL);
+    printf("Zip: %p\n", zip);
     return 0;
 }
