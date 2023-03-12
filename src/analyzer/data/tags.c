c2f1bb6b4382773506120c77f531962de4791f49
EpicPix <epicpixminecraft@gmail.com>
2023-03-12 22:18:26 +0100

    Check `minecraft` tags if they are supported in the version

--- src/analyzer/data/tags.c
+++ src/analyzer/data/tags.c
@@ -1,6 +1,7 @@
 #include "data.h"
 #include "../../loader.h"
 #include "../namespace.h"
+#include <string.h>
 
 void load_tags_block(zip_t* zip, const char* namespace, const char* filename, struct analysis_data *data, struct analyzer_results *results) {
 
@@ -11,7 +12,13 @@ void load_tags_entity_type(zip_t* zip, const char* namespace, const char* filena
 };
 
 void load_tags_function(zip_t* zip, const char* namespace, const char* filename, struct analysis_data *data, struct analyzer_results *results) {
-
+    if(strcmp(namespace, "minecraft") == 0) {
+      if(strcmp(filename, "/tags/functions/load.json") == 0) {
+        diagnostic_create_source_dyn_range_clean(results, diagnostic_error, clone_string("Unable to use the load tag in this version"), namespace_file_string(namespace, "tags/functions/load.json"), version_index("17w49a"), version_index("18w01a"));
+      }else if(strcmp(filename, "/tags/functions/tick.json") == 0) {
+        diagnostic_create_source_dyn_range_clean(results, diagnostic_error, clone_string("Unable to use the tick tag in this version"), namespace_file_string(namespace, "tags/functions/tick.json"), version_index("17w49a"), version_index("17w49b"));
+      }
+    }
 };
 
 void load_tags_item(zip_t* zip, const char* namespace, const char* filename, struct analysis_data *data, struct analyzer_results *results) {
