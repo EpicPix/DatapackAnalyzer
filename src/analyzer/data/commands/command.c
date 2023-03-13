8f12eed367e1bc047405e6efd677f3032ec2bdaf
EpicPix <epicpixminecraft@gmail.com>
2023-03-13 10:39:31 +0100

    How about not showing every command

--- src/analyzer/data/commands/command.c
+++ src/analyzer/data/commands/command.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 void load_command(zip_t* zip, const char* namespace_name, const char* filename, int line_number, const char* line, int line_length, struct analysis_data *data, struct analyzer_results *results) {
-    diagnostic_clean(
-      diagnostic_create_source_loc_dyn(diagnostic_error, "Invalid command", namespace_file_string(namespace_name, filename + 1), line_number, -1),
-      analyzer_add_diagnostic_all(results, diagnostic)
-    );
+//    diagnostic_clean(
+//      diagnostic_create_source_loc_dyn(diagnostic_error, "Invalid command", namespace_file_string(namespace_name, filename + 1), line_number, -1),
+//      analyzer_add_diagnostic_all(results, diagnostic)
+//    );
 };
 
 void load_commands(zip_t* zip, const char* namespace_name, const char* filename, const char* content, struct analysis_data *data, struct analyzer_results *results) {
