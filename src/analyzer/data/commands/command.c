5696278ff72c2f49587909735f5195799e18545b
EpicPix <epicpixminecraft@gmail.com>
2023-03-23 18:24:11 +0100

    I feel like creating an AST for commands is a mistake

--- src/analyzer/data/commands/command.c
+++ src/analyzer/data/commands/command.c
@@ -1,4 +1,5 @@
 #include "commands.h"
+#include "command_ast.h"
 #include "../../namespace.h"
 #include <string.h>
 
@@ -8,9 +9,9 @@
 #define COMMAND_DIAGNOSTIC(CONTEXT, TYPE, VALUE) \
   analyzer_add_diagnostic_range_msg_file_loc(CONTEXT->results, TYPE, VALUE, namespace_file_string(CONTEXT->namespace_name, CONTEXT->filename + 1), CONTEXT->line_number, CONTEXT->column, NULL, NULL)
 
-#define COMMAND(NAME) void load_command_##NAME(zip_t* zip, struct command_load_context* context)
-#define CHECK_COMMAND(NAME) if(command_length == strlen(#NAME) && memcmp(line, #NAME, command_length) == 0) { load_command_##NAME(zip, context); return; }
-#define CHECK_COMMAND_ALIAS(ALIAS, NAME) if(command_length == strlen(#ALIAS) && memcmp(line, #ALIAS, command_length) == 0) { load_command_##NAME(zip, context); return; }
+#define COMMAND(NAME) command_ast* load_command_##NAME(zip_t* zip, struct command_load_context* context)
+#define CHECK_COMMAND(NAME) if(command_length == strlen(#NAME) && memcmp(line, #NAME, command_length) == 0) { return load_command_##NAME(zip, context); }
+#define CHECK_COMMAND_ALIAS(ALIAS, NAME) if(command_length == strlen(#ALIAS) && memcmp(line, #ALIAS, command_length) == 0) { return load_command_##NAME(zip, context); }
 
 struct command_load_context {
   const char* namespace_name;
@@ -24,257 +25,277 @@ struct command_load_context {
 };
 
 COMMAND(advancement) {
-
+  return NULL;
 }
 
 COMMAND(attribute) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `attribute` is not supported in this version", NULL, "20w17a");
+  return NULL;
 }
 
 COMMAND(blockdata) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `blockdata` is not supported in this version, use `data block`", "17w45b", NULL);
+  return NULL;
 }
 
 COMMAND(bossbar) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `bossbar` is not supported in this version", NULL, "18w05a");
+  return NULL;
 }
 
 COMMAND(clear) {
-
+  return NULL;
 }
 
 COMMAND(clone) {
-
+  return NULL;
 }
 
 COMMAND(damage) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `damage` is not supported in this version", NULL, "23w06a");
+  return NULL;
 }
 
 COMMAND(data) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `data` is not supported in this version", NULL, "17w45b");
+  return NULL;
 }
 
 COMMAND(datapack) {
-
+  return NULL;
 }
 
 COMMAND(defaultgamemode) {
-
+  return NULL;
 }
 
 COMMAND(difficulty) {
-
+  return NULL;
 }
 
 COMMAND(drop) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `drop` is not supported in this version", NULL, "18w43a");
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `drop` is not supported in this version, use `loot`", "18w45a", NULL);
+  return NULL;
 }
 
 COMMAND(effect) {
-
+  return NULL;
 }
 
 COMMAND(enchant) {
-
+  return NULL;
 }
 
 COMMAND(entitydata) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `entitydata` is not supported in this version, use `data entity`", "17w45b", NULL);
+  return NULL;
 }
 
 COMMAND(execute) {
-
+  return NULL;
 }
 
 COMMAND(experience) {
-
+  return NULL;
 }
 
 COMMAND(fill) {
-
+  return NULL;
 }
 
 COMMAND(fillbiome) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `fillbiome` is not supported in this version", NULL, "22w46a");
+  return NULL;
 }
 
 COMMAND(forceload) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `forceload` is not supported in this version", NULL, "1.14.4-pre4");
+  return NULL;
 }
 
 COMMAND(function) {
-
+  return NULL;
 }
 
 COMMAND(gamemode) {
-
+  return NULL;
 }
 
 COMMAND(gamerule) {
-
+  return NULL;
 }
 
 COMMAND(give) {
-
+  return NULL;
 }
 
 COMMAND(help) {
-
+  return NULL;
 }
 
 COMMAND(item) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `item` is not supported in this version", NULL, "20w46a");
+  return NULL;
 }
 
 COMMAND(kill) {
-
+  return NULL;
 }
 
 COMMAND(list) {
-
+  return NULL;
 }
 
 COMMAND(locate) {
-
+  return NULL;
 }
 
 COMMAND(locatebiome) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `locatebiome` is not supported in this version", NULL, "20w06a");
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `locatebiome` is not supported in this version, use `locate biome`", "22w19a", NULL);
+  return NULL;
 }
 
 COMMAND(loot) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `loot` is not supported in this version", NULL, "18w45a");
+  return NULL;
 }
 
 COMMAND(me) {
-
+  return NULL;
 }
 
 COMMAND(particle) {
-
+  return NULL;
 }
 
 COMMAND(place) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `place` is not supported in this version", NULL, "22w18a");
+  return NULL;
 }
 
 COMMAND(placefeature) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `placefeature` is not supported in this version", NULL, "22w03a");
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `placefeature` is not supported in this version, use `place feature`", "22w18a", NULL);
+  return NULL;
 }
 
 COMMAND(playsound) {
-
+  return NULL;
 }
 
 COMMAND(recipe) {
-
+  return NULL;
 }
 
 COMMAND(reload) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `reload` is not supported in this version", NULL, "1.14.4-pre4");
+  return NULL;
 }
 
 COMMAND(replaceitem) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `replaceitem` is not supported in this version, use `item replace`", "20w46a", NULL);
+  return NULL;
 }
 
 COMMAND(ride) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `ride` is not supported in this version", NULL, "23w03a");
+  return NULL;
 }
 
 COMMAND(say) {
-
+  return NULL;
 }
 
 COMMAND(schedule) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `scnedule` is not supported in this version", NULL, "18w43a");
+  return NULL;
 }
 
 COMMAND(scoreboard) {
-
+  return NULL;
 }
 
 COMMAND(seed) {
-
+  return NULL;
 }
 
 COMMAND(setblock) {
-
+  return NULL;
 }
 
 COMMAND(setworldspawn) {
-
+  return NULL;
 }
 
 COMMAND(spawnpoint) {
-
+  return NULL;
 }
 
 COMMAND(spectate) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `spectate` is not supported in this version", NULL, "19w41a");
+  return NULL;
 }
 
 COMMAND(spreadplayers) {
-
+  return NULL;
 }
 
 COMMAND(stopsound) {
-
+  return NULL;
 }
 
 COMMAND(summon) {
-
+  return NULL;
 }
 
 COMMAND(tag) {
-
+  return NULL;
 }
 
 COMMAND(team) {
-
+  return NULL;
 }
 
 COMMAND(teammsg) {
   COMMAND_DIAGNOSTIC_RANGE(context, diagnostic_error, "Command `teammsg` is not supported in this version", NULL, "19w02a");
+  return NULL;
 }
 
 COMMAND(teleport) {
-
+  return NULL;
 }
 
 COMMAND(tell) {
-
+  return NULL;
 }
 
 COMMAND(tellraw) {
-
+  return NULL;
 }
 
 COMMAND(time) {
-
+  return NULL;
 }
 
 COMMAND(title) {
-
+  return NULL;
 }
 
 COMMAND(trigger) {
-
+  return NULL;
 }
 
 COMMAND(weather) {
-
+  return NULL;
 }
 
 COMMAND(worldborder) {
-
+  return NULL;
 }
 
-void load_command(zip_t* zip, struct command_load_context* context) {
+command_ast* load_command(zip_t* zip, struct command_load_context* context) {
   struct analyzer_results *results = context->results;
   const char* line = context->line;
 
@@ -356,6 +377,7 @@ void load_command(zip_t* zip, struct command_load_context* context) {
   CHECK_COMMAND_ALIAS(xp, experience);
 
   COMMAND_DIAGNOSTIC(context, diagnostic_error, "Command is not supported");
+  return NULL;
 };
 
 void load_commands(zip_t* zip, const char* namespace_name, const char* filename, char* content, struct analysis_data *data, struct analyzer_results *results) {
@@ -393,7 +415,7 @@ void load_commands(zip_t* zip, const char* namespace_name, const char* filename,
           .data = data,
           .results = results,
       };
-      load_command(zip, &context);
+      command_ast* ast = load_command(zip, &context);
       content[end] = endChar;
     }
   }
---
+++ src/analyzer/data/commands/command_ast.h
@@ -0,0 +1,5 @@
+#pragma once
+
+typedef struct command_ast {
+  
+} command_ast;
