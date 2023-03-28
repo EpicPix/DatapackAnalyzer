b34eaf5808f2c40eede37fc8522f5a8c88737a26
EpicPix <epicpixminecraft@gmail.com>
2023-03-28 17:24:51 +0200

    Add printing filenames when debugging memory

--- src/memory.c
+++ src/memory.c
@@ -4,42 +4,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifndef PRINT_ALLOC
 void* zmalloc(uint64_t length) {
-#ifdef PRINT_ALLOC
-  fprintf(stderr, "zmalloc(%lu) = ", length);
-#endif
+  return malloc(length);
+}
+#else
+void* zmalloc(uint64_t length, const char* file, int line) {
+  fprintf(stderr, "%s:%d zmalloc(%lu) = ", file, line, length);
   void* ptr = malloc(length);
-#ifdef PRINT_ALLOC
   fprintf(stderr, "%p\n", ptr);
-#endif
   return ptr;
 }
-
-void zfree(void* location) {
-#ifdef PRINT_ALLOC
-  fprintf(stderr, "zfree(%p)\n", location);
 #endif
+
+#ifndef PRINT_ALLOC
+void zfree(void* location) {
   free(location);
 }
+#else
+void zfree(void* location, const char* file, int line) {
+  fprintf(stderr, "%s:%d zfree(%p)\n", file, line, location);
+  free(location);
+}
+#endif
 
+#ifndef PRINT_ALLOC
 void* zcalloc(uint64_t length) {
-#ifdef PRINT_ALLOC
-  fprintf(stderr, "zcalloc(%lu) = ", length);
-#endif
+  return calloc(length, 1);
+}
+#else
+void* zcalloc(uint64_t length, const char* file, int line) {
+  fprintf(stderr, "%s:%d zcalloc(%lu) = ", file, line, length);
   void* ptr = calloc(length, 1);
-#ifdef PRINT_ALLOC
   fprintf(stderr, "%p\n", ptr);
-#endif
   return ptr;
 }
+#endif
 
+
+#ifndef PRINT_ALLOC
 void* zrealloc(void* location, uint64_t length) {
-#ifdef PRINT_ALLOC
-  fprintf(stderr, "zrealloc(%p, %lu) = ", location, length);
-#endif
+  return realloc(location, length);
+}
+#else
+void* zrealloc(void* location, uint64_t length, const char* file, int line) {
+  fprintf(stderr, "%s:%d zrealloc(%p, %lu) = ", file, line, location, length);
   void* ptr = realloc(location, length);
-#ifdef PRINT_ALLOC
   fprintf(stderr, "%p\n", ptr);
-#endif
   return ptr;
 }
+#endif
--- src/memory.h
+++ src/memory.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+#ifndef PRINT_ALLOC
+
 #define MALLOC(N) zmalloc(N)
 #define CALLOC(N) zcalloc(N)
 #define REALLOC(LOC, N) zrealloc(LOC, N)
@@ -11,3 +13,17 @@ void* zmalloc(uint64_t length);
 void* zcalloc(uint64_t length);
 void* zrealloc(void* old, uint64_t length);
 void zfree(void* ptr);
+
+#else
+
+#define MALLOC(N) zmalloc(N, __FILE__, __LINE__)
+#define CALLOC(N) zcalloc(N, __FILE__, __LINE__)
+#define REALLOC(LOC, N) zrealloc(LOC, N, __FILE__, __LINE__)
+#define FREE(PTR) zfree(PTR, __FILE__, __LINE__)
+
+void* zmalloc(uint64_t length, const char* file, int line);
+void* zcalloc(uint64_t length, const char* file, int line);
+void* zrealloc(void* old, uint64_t length, const char* file, int line);
+void zfree(void* ptr, const char* file, int line);
+
+#endif
